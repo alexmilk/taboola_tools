c390244fbf63939bd3bc7531e1f1b342a37be6d1
alex.m <alex.m@taboola.com>
2019-03-06 23:06:05 +0200

    Added finished infra

---
+++ js/curling.js
@@ -0,0 +1,35 @@
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//The page functions script                                  ||
+//Created by Alex Milkis - 2019 Taboola                      ||
+//The MIT License                                            ||
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+const ele = document.getElementsByClassName('form-control');function runInt() {
+    let ss = 0, howManyTimes = ele[2].value;
+    function f() {
+        runSync();
+        ss++;
+        if (ss < howManyTimes) {
+            setTimeout(f, ele[3].value);
+        }
+    }
+
+    f();return false;
+}
+function runSync ()  {
+    let r = [10000, 20000];
+    let m = Math.floor(Math.random() * (+r[1] - +r[0])) + +r[0];
+    $.ajax({
+        type: 'GET',
+        url: ele[1].value + m,
+        processData: false,
+        dataType: 'text', // data type expected from server
+        success: function (data) {
+            $(".form-control")[0].append(data);
+        },
+        error: function () {
+            $(".form-control")[0].append(data);
+        },
+    });
+    return false;
+}
